47c0cefe6c1b07106dd3efa3565daf5d5d2ffb3c
fernando gomes <fernando.gmsoli@gmail.com>
2021-07-27 18:15:44 -0300

    Update reference firebase

--- app/src/main/java/com/example/whatszappy/config/ConfigFirebase.java
+++ app/src/main/java/com/example/whatszappy/config/ConfigFirebase.java
@@ -4,12 +4,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ConfigFirebase {
 
     private static FirebaseAuth authentication;
     private static DatabaseReference database;
-    private static FirebaseUser userFB;
+    private static StorageReference storage;
 
     //retorna a instancia do firebaseDatabase
     public static DatabaseReference getFirebaseDatabase(){
@@ -26,6 +28,13 @@ public class ConfigFirebase {
             authentication = FirebaseAuth.getInstance();
         }
         return authentication;
+    }
+
+    public static StorageReference getFirebaseStorage(){
+        if ( storage == null){
+            storage = FirebaseStorage.getInstance().getReference();
+        }
+        return storage;
     }
 
 
